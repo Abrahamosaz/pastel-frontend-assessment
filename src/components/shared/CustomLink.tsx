51b2642c1f0bc708a99dfe25383bd9565f70c8a0
abraham <abrahamosazee3@gmail.com>
2025-04-18 06:31:04 +0100

    clean  up, remove unused components

--- src/components/shared/CustomLink.tsx
+++
@@ -1,8 +0,0 @@
-import { CustomLinkProps } from "@/types";
-import React from "react";
-
-const CustomLink: React.FC<CustomLinkProps> = ({}) => {
-  return <div>CustomLink</div>;
-};
-
-export default CustomLink;
--- src/components/shared/Typography.tsx
+++
@@ -1,8 +0,0 @@
-import { TypographyProps } from "@/types";
-import React from "react";
-
-const Typography: React.FC<TypographyProps> = ({}) => {
-  return <div className="w-full"></div>;
-};
-
-export default Typography;
